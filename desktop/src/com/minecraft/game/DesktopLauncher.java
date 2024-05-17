9fa7599a0f53633ef00def1b01422efd228d924c
Coquixo <coquixo98@gmail.com>
2024-05-17 22:30:53 +0200

    git innit

---
+++ desktop/src/com/minecraft/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.minecraft.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.minecraft.game.MinecraftGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Minecraft Game");
+		new Lwjgl3Application(new MinecraftGame(), config);
+	}
+}
